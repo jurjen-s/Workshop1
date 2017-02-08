4e3edf2c3aa0a99e5a452056edb89099ed88902b
FRANK <lorus1@live.nl>
2017-02-08 15:54:07 +0100

    lk vind dit niks.

--- src/main/java/rsvier/workshop1/Inlogmenu.java
+++ src/main/java/rsvier/workshop1/Inlogmenu.java
@@ -3,104 +3,61 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package opdracht6test;
+package rsvier.workshop1;
 
 /**
  *
- * @author Frank
+ * @author jurjen
  */
-public class Inlogmenu {
+public class InlogMenu {
     
-  public static int inlogMenu(){
-      int cijfer;
-      
-      
-      System.out.println("Log in");
-      System.out.println("1: inloggen ");
-      System.out.println("2: account aanmaken ");
-      System.out.println("0: stoppen");
-      
-      cijfer = TextIO.getlnInt();
-      
-if (cijfer == 0){ System.out.println("Einde"); cijfer = 0;}
-else if (cijfer <= 2 && cijfer >0){
-    switch (cijfer){
-        case 1: System.out.println("U gaat inloggen");inlogControle(); cijfer = 1;   break;
-        case 2: System.out.println("U gaat een account aanmaken"); inlogAccountAanmaken(); cijfer = 2; break;
-     
+    public void inloggen() {
+    System.out.println("U wilt inloggen.");
+    System.out.println("Optie 1: Inloggen.");
+    System.out.println("Optie 2: Account aanmaken");
+    System.out.println("Optie 0: Stoppen.");
+    int keuze = TextIO.getlnInt();
+        switch (keuze) {
+            case 1: inlogMenu();
+                    break;
+            case 2: aanmaakMenu();
+                    break;
+            case 0: System.exit();        
+        }
     }
-}
-else{ System.out.println("foute cijfer waarde probeer opnieuw"); inlogMenu(); }
-
-return cijfer;
-  }
-      
-      
-   public static void inlogControle(){
-       
-       System.out.println("vul account id in");
-        int AcId = TextIO.getlnInt();
+    
+    public void inlogMenu() {
+        System.out.println("Vul uw id in: ");
+        int accountId = TextIO.getlnInt();
+        System.out.println("Vul uw wachtwoord in: ");
+        String wachtwoord = TextIO.getln();
+        /*
+        if (controleerLogin(accountId, wachtwoord) {
+            hoofdmenu();
+        } else {
+            inloggen();
+        }
+        */
+        if (true) {
+            hoofdmenu();
+        } else {
+            inloggen();
+        }
+    }
+    
+    public void aanmaakMenu() {
+        System.out.println("Vul uw account typ in: ");
+        int accountType = TextIO.getlnInt();
+        System.out.println("Vul uw wachtwoord in: ");
+        String wachtwoord = TextIO.getln();
+        // roep functie aan die account aanmaakt in db, verkrijg ID
+        // print id, type en wachtwoord en keer dan terug naar inloggen
+        inloggen();
+    }    
+    
+    public void hoofdmenu() {
+        System.out.println("Hoera!");
+    }
         
-       System.out.println("vul wachtwoord in");
-       
-       String wachtwoord = TextIO.getln();
-       
-       
-       // controle uitvoeren met database, als dat goed is , -> niks-> cijfer is 1 -> toestemming menu.
-  
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       // ALS dat fout is terug, dus inlogMenu();
-       
-       
-       //Inlogmenu.inlogMenu();
-       
-       
-   }   
-      
-      public static void inlogAccountAanmaken(){
-          
-          System.out.println("u krijgt een automatische inlog id");
-        
-        
-       System.out.println("vul u eigen wachtwoord in");
-       
-       String wachtwoord = TextIO.getln();
-          
-       
-       
-       
-         System.out.println("uw id is :" + "SQL code ontbreekt nog");
-         System.out.println("uw wachtwoord is : "+ wachtwoord);
-          
-         // data in sql opslaan en id terug weergeven!!
-         
-         //dan terug naar loginmenu ALTIJD
-         
-         Inlogmenu.inlogMenu();
-         
-          
-      }
-   
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
