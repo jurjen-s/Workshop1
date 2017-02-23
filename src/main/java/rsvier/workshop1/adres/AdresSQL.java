d5df1cd2702bc933ebb4bf9519767565dfd7dd51
FRANK <lorus1@live.nl>
2017-02-23 11:41:28 +0100

    AdresSQL abstract af.

--- src/main/java/rsvier/workshop1/adres/AdresSQL.java
+++ src/main/java/rsvier/workshop1/adres/AdresSQL.java
@@ -5,10 +5,33 @@
  */
 package rsvier.workshop1.adres;
 
+import java.util.List;
+
 /**
  *
  * @author Frank
  */
-public class AdresSQL {
+public abstract class AdresSQL {
+    
+    
+    abstract Adres findAdresById(int adresId);
+    abstract List findAdresByType(int adresType);
+    abstract List findAdresByKlantId(int klantId);
+    abstract List findAdresByPostcode(String postcode);
+    abstract List findAdresByLand(String land);
+    
+    abstract Adres toevoegenAdres(Adres adres);
+    abstract boolean deleteAdres(int AdresID);
+    
+    abstract boolean updateAdresType(int adresId, int adresType);
+    abstract boolean updateAdresStraatnaam(int adresId, String straatnaam);
+    abstract boolean updateAdresHuisnummer(int adresId, int huisnummer);
+    abstract boolean updateAdresHeeftHuisnrToevoeging(int adresId, int heeftHuisnrToevoeging);
+    abstract boolean updateAdresHuisnrToevoeging(int adresId, String huisnrToevoeging);
+    abstract boolean updateAdresPostcode(int adresId, String postcode);
+    abstract boolean updateAdresLand(int adresId, String land);
+    
+    
+    
     
 }
