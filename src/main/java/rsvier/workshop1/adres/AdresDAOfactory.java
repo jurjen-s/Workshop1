6b57186d2c8d9eba2bdc1728925711a2aa05accd
FRANK <lorus1@live.nl>
2017-02-23 11:56:08 +0100

    AdresDAOfactory af met oude sqlconnection.

--- src/main/java/rsvier/workshop1/adres/AdresDAOfactory.java
+++ src/main/java/rsvier/workshop1/adres/AdresDAOfactory.java
@@ -5,10 +5,77 @@
  */
 package rsvier.workshop1.adres;
 
+import java.sql.Connection;
+import rsvier.workshop1.account.AccountFireBirdSQL;
+import rsvier.workshop1.account.AccountMySQL;
+import rsvier.workshop1.account.AccountSQL;
+import rsvier.workshop1.db.DbConfigurator;
+import rsvier.workshop1.db.SQLConnection;
+
 /**
  *
  * @author Frank
  */
 public class AdresDAOfactory {
+      //Hier komt de DAO factory voor de keuze firebird of mysql.
     
+   SQLConnection connectie= new SQLConnection();
+   Connection sqlconnectie = connectie.getSQLConnection();
+   
+    
+    
+    
+     public AdresSQL getAdresDAO(){
+         
+         //TESTSTRING!:
+         //String type = "MySQL";
+         
+         
+         //DIT MOET NIET VERWIJDERD WORDEN
+        
+         
+         
+         
+        
+        String type = DbConfigurator.getDbConfigurator().getDbType();
+        System.out.println(type);
+        
+        //geeft de string "MySQL" of "Firebird" uit de XML bestand.
+        
+        
+        
+        label:
+        while(true){
+        if (type.equalsIgnoreCase("MYSQL")) {
+           System.out.println("MySQL is de database");
+             AdresSQL mysql = new AdresMySQL(sqlconnectie);
+             
+             return mysql;
+             //return new AccountMySQLSQL;  
+          //  return new AccountSQL();    
+        } else if (type.equalsIgnoreCase("Firebird")) {
+            System.out.println("Firebird is de database");
+                return new AdresFireBirdSQL();
+        }
+        else{ //Zoekt een default
+        System.out.println("geen SQL config gegeven: type Firebird of MySQL of overige");
+        System.out.println("We nemen de default db met MySQL");
+          type =  "";
+          
+        DbConfigurator dbConfigurator = DbConfigurator.getDbConfigurator();
+        
+        
+        dbConfigurator.setDatabase(type);
+        
+        type = dbConfigurator.getDbType();
+        
+        }
+        }
+     }
 }
+        
+        
+         
+       
+        
+
