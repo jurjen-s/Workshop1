9c00f996d85d30b0ae9081c1c9e167d1984f0e70
FRANK <lorus1@live.nl>
2017-02-23 11:53:01 +0100

    extend met AdresSQL en werken nu samen straks voor de DAOfactory.

--- src/main/java/rsvier/workshop1/adres/AdresFireBirdSQL.java
+++ src/main/java/rsvier/workshop1/adres/AdresFireBirdSQL.java
@@ -5,10 +5,82 @@
  */
 package rsvier.workshop1.adres;
 
+import java.util.List;
+
 /**
  *
  * @author Frank
  */
-public class AdresFireBirdSQL {
+public class AdresFireBirdSQL extends AdresSQL implements AdresDAO {
+
+    @Override
+    public Adres findAdresById(int adresId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List findAdresByType(int adresType) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List findAdresByKlantId(int klantId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List findAdresByPostcode(String postcode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List findAdresByLand(String land) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Adres toevoegenAdres(Adres adres) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteAdres(int AdresID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateAdresType(int adresId, int adresType) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateAdresStraatnaam(int adresId, String straatnaam) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateAdresHuisnummer(int adresId, int huisnummer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateAdresHeeftHuisnrToevoeging(int adresId, int heeftHuisnrToevoeging) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateAdresHuisnrToevoeging(int adresId, String huisnrToevoeging) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateAdresPostcode(int adresId, String postcode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateAdresLand(int adresId, String land) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
